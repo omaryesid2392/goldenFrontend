b22ee4d14529adeff50c65283163bfcf7d4e064a
OMAR YESID CORRALES HERNANDEZ <omaryesid@MacBook-Pro-de-OMAR.local>
2021-12-01 19:57:24 -0500

    Deploy Frontend

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/goldenFrontend'));
+app.use(express.static(__dirname + '/dist/Golden'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/goldenFrontend/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Golden/index.html'));
 });
 
 // default Heroku port
